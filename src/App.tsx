c7fa657307a3a579907b69befd764ee168859e03
Luis Esteban R <leramirezca@gmail.com>
2024-02-20 17:40:51 -0500

    fix image in deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import Card from "./components/card";
 
 const CARD_DATA = {
-  image: "/public/assets/images/illustration-article.svg",
+  image: "assets/images/illustration-article.svg",
   published: "21 December 2023",
   title: "HTML & CSS foundations",
-  description: 'These languages are backbone for every website, defining structure, content and presentation',
+  description:
+    "These languages are backbone for every website, defining structure, content and presentation",
   author: {
-    avatar: "/public/assets/images/image-avatar.webp",
-    name: "Michelle Appleton"
-  }
+    avatar: "/assets/images/image-avatar.webp",
+    name: "Michelle Appleton",
+  },
 };
 
 function App() {
   return (
-    <div className="flex w-screen h-screen items-center justify-center bg-primary">
+    <div className="flex h-screen w-screen items-center justify-center bg-primary">
       <Card cardData={CARD_DATA} />
     </div>
   );
