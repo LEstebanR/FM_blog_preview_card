081a5fac03ad77a1402fbee5b00dc7986541748c
Luis Esteban R <leramirezca@gmail.com>
2024-02-20 17:35:46 -0500

    add tailwind plugin and fix image size

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -16,8 +16,13 @@ type props = {
 
 const Card: React.FC<props> = ({ cardData }) => {
   return (
-    <div className="flex h-4/6 max-h-100 w-11/12 max-w-96 flex-col items-center justify-around rounded-lg border bg-white p-4 shadow-card hover:shadow-hover">
-      <img className="h-3/6 rounded-lg" src={cardData.image} />
+    <div className="flex h-4/6 max-h-100 w-11/12 max-w-96 flex-col items-center justify-around rounded-3xl border bg-white p-4 shadow-card hover:shadow-hover">
+      <img
+        className="rounded-xl"
+        src={cardData.image}
+        alt="card image"
+        height={200}
+      />
       <div className="flex h-3/6 w-full flex-col items-start justify-around">
         <Button label="Learning" />
         <p>Published {cardData.published}</p>
